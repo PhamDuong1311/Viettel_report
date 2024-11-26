88dd248b5e7a1ebf14e6c6b6008a7e437a0b9f1f
PhamDuong1311 <131937109+PhamDuong1311@users.noreply.github.com>
2024-11-26 10:22:34 +0000

    updated

---
+++ UDP_Socket/server.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+
+
+int main() {
+    int server_socket;
+    struct sockaddr_in server_addr;
+
+    server_socket = socket(AF_INET, SOCK_)
+
+    return 0;
+}
